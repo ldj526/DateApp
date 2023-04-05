2299a0581eaaf77c3b5d4f7eeba347500ec1e88e
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-05 22:46:20 +0900

    feat: 회원가입 페이지 디자인

---
+++ app/src/main/java/com/example/dateapp/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dateapp.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dateapp.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
