742d78e8674252bb6c7a61a59dfa8df054cff3c2
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-05 23:16:06 +0900

    feat: Firebase 기본 설정

--- app/src/main/java/com/example/dateapp/auth/JoinActivity.kt
+++ app/src/main/java/com/example/dateapp/auth/JoinActivity.kt
@@ -1,12 +1,40 @@
 package com.example.dateapp.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.dateapp.MainActivity
 import com.example.dateapp.R
+import com.example.dateapp.databinding.ActivityJoinBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityJoinBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        binding.joinBtn.setOnClickListener {
+            val email = binding.email.text.toString()
+            val pwd = binding.pwd.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+
+                    }
+                }
+        }
     }
 }
