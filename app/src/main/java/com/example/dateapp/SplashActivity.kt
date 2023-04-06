fd8e3b4b401a75c9184fe885d2f72662480f11b9
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-06 18:28:21 +0900

    feat: Token 값을 생성하여 특정 사용자에게만 메세지 보여주는 기능

--- app/src/main/java/com/example/dateapp/SplashActivity.kt
+++ app/src/main/java/com/example/dateapp/SplashActivity.kt
@@ -1,18 +1,38 @@
 package com.example.dateapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dateapp.auth.IntroActivity
 import com.example.dateapp.utils.FirebaseAuthUtils
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.FirebaseAuthKtxRegistrar
+import com.google.firebase.messaging.FirebaseMessaging
 
 class SplashActivity : AppCompatActivity() {
+
+    private val TAG = SplashActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Token
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            Log.d(TAG, token.toString())
+        })
 
         // 현재 uid 값 가져오기
         val uid = FirebaseAuthUtils.getUid()
